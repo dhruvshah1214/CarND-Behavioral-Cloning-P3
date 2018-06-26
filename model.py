0ce971cf6494919207ab7b7aa3cc6795192c4d9a
Dhruv Shah <dhruv.shah@gmail.com>
2018-06-26 22:51:40 +0000

    first model

--- model.py
+++ model.py
@@ -5,9 +5,12 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Convolution2D, Lambda
 from keras.regularizers import l2
 import tensorflow as tf
+from keras.preprocessing import image as keras_image
 
-def rgb2yuv(x):
-    return tf.image.rgb_to_yuv(x)
+def preprocess(x):
+	arr = x #keras_image.img_to_array(x)
+	yuv_arr = cv2.resize(cv2.cvtColor(arr, cv2.COLOR_RGB2YUV), (160, 80))
+	return yuv_arr
 
 lines = []
 with open('data/driving_log.csv') as csvfile:
@@ -26,7 +29,7 @@ for line in lines:
 	full_path = 'data/IMG/' + filename
 	image = cv2.imread(full_path)
 	image_rgb = image[...,::-1]
-	images.append(image_rgb)
+	images.append(preprocess(image_rgb))
 	measurement_steer = float(line[3])
 	steer.append(measurement_steer)
 
@@ -38,20 +41,18 @@ keep_prob = 0.5
 activation_func = 'relu'
 
 model = Sequential()
-model.add(Lambda(rgb2yuv, input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: cv2.resize(x, (80, 160))))
-model.add(Lambda(lambda x: x/255.0 - 0.5))
-model.add(Convolution2D(24, 5, 5, activation=activation_func, subsample=(2, 2), W_regulizer=l2(reg_constant)))
-model.add(Convolution2D(36, 5, 5, activation=activation_func, subsample=(2, 2), W_regulizer=l2(reg_constant)))
-model.add(Convolution2D(48, 5, 5, activation=activation_func, subsample=(2, 2), W_regulizer=l2(reg_constant)))
-model.add(Convolution2D(64, 3, 3, activation=activation_func, W_regulizer=l2(reg_constant)))
-model.add(Convolution2D(64, 3, 3, activation=activation_func, W_regulizer=l2(reg_constant)))
+model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(80,160,3)))
+model.add(Convolution2D(24, 5, 5, activation=activation_func, subsample=(2, 2), W_regularizer=l2(reg_constant)))
+model.add(Convolution2D(36, 5, 5, activation=activation_func, subsample=(2, 2), W_regularizer=l2(reg_constant)))
+model.add(Convolution2D(48, 5, 5, activation=activation_func, subsample=(2, 2), W_regularizer=l2(reg_constant)))
+model.add(Convolution2D(64, 3, 3, activation=activation_func, W_regularizer=l2(reg_constant)))
+model.add(Convolution2D(64, 3, 3, activation=activation_func, W_regularizer=l2(reg_constant)))
 model.add(Flatten())
-model.add(Dense(1164, activation=activation_func, W_regulizer=l2(reg_constant)))
-model.add(Dense(100, activation=activation_func, W_regulizer=l2(reg_constant)))
-model.add(Dense(50, activation=activation_func, W_regulizer=l2(reg_constant)))
-model.add(Dense(10, activation=activation_func, W_regulizer=l2(reg_constant)))
-model.add(Dense(1, activation=activation_func, W_regulizer=l2(reg_constant)))
+model.add(Dense(1164, activation=activation_func, W_regularizer=l2(reg_constant)))
+model.add(Dense(100, activation=activation_func, W_regularizer=l2(reg_constant)))
+model.add(Dense(50, activation=activation_func, W_regularizer=l2(reg_constant)))
+model.add(Dense(10, activation=activation_func, W_regularizer=l2(reg_constant)))
+model.add(Dense(1, activation=activation_func, W_regularizer=l2(reg_constant)))
 
 
 model.compile(loss='mse', optimizer='adam')
