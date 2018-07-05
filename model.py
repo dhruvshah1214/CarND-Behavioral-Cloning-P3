c23584f9e0025da49124c30218d89ab7a3db413b
Dhruv Shah <dhruv.shah@gmail.com>
2018-07-04 22:51:18 -0700

    dataug

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Convolution2D, Lambda
+from keras.layers import Flatten, Dense, Convolution2D, Lambda, Dropout
 from keras.regularizers import l2
 import tensorflow as tf
 from keras.preprocessing import image as keras_image
@@ -32,10 +32,40 @@ if __name__ == '__main__':
 		full_path = 'data/IMG/' + filename
 		#print(full_path)
 		image = cv2.imread(full_path)
+
 		image_rgb = image[...,::-1]
-		images.append(preprocess(image_rgb))
 		measurement_steer = float(line[3])
+		images.append(preprocess(image_rgb))
 		steer.append(measurement_steer)
+
+		if steer != 0:
+
+			steer_correction = 0.2
+
+			path_left = 'data/IMG/' + line[1].split('/')[-1]
+			img_left = cv2.imread(path_left)
+			img_left_rgb = img_left[...,::-1]
+
+			images.append(preprocess(img_left_rgb))
+			steer.append(measurement_steer + steer_correction)
+
+			path_right = 'data/IMG/' + line[2].split('/')[-1]
+			img_right = cv2.imread(path_right)
+			img_right_rgb = img_right[..., ::-1]
+
+			images.append(preprocess(img_right_rgb))
+			steer.append(measurement_steer - steer_correction)
+
+
+	for (img, steer_val) in zip(images, steer):
+		if steer_val != 0:
+			img_flip = np.fliplr(img)
+			measurement_flip = -steer_val
+			images.append(preprocess(img_flip))
+			steer.append(measurement_flip)
+
+
+
 
 	X_train = np.array(images)
 	y_train = np.array(steer)
@@ -52,6 +82,7 @@ if __name__ == '__main__':
 	model.add(Convolution2D(64, 3, 3, activation=activation_func))
 	model.add(Convolution2D(64, 3, 3, activation=activation_func))
 	model.add(Flatten())
+	model.add(Dropout(0.5))
 	model.add(Dense(1164, activation=activation_func))
 	model.add(Dense(100, activation=activation_func))
 	model.add(Dense(50, activation=activation_func))
